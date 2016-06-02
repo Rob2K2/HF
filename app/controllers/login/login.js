6080e7a61ae1c8943d4052a0a238e02ddf25f1b9
Rob <merino@live.com>
2016-06-02 15:30:15 -0400

    Reparado boton de salida de la app

--- app/controllers/login/login.js
+++ app/controllers/login/login.js
@@ -4,26 +4,33 @@ var NavGroupModule = Alloy.Globals.NavGroupModule;
 var navGroupModule = new NavGroupModule($.login);
 
 function doClick(e) {
-    
-    //Ti.API.info($.username.value + ' - ' + $.password.value);
-    var arg = {
-        containingWindow: navGroupModule    
-    };
-    var searchWindow = Alloy.createController('search/search', arg).getView();
-    navGroupModule.openWindow(searchWindow, false);
-    
+
+	//Ti.API.info($.username.value + ' - ' + $.password.value);
+	var arg = {
+		containingWindow : navGroupModule
+	};
+	var searchWindow = Alloy.createController('search/search', arg).getView();
+	navGroupModule.openWindow(searchWindow, false);
+
 }
 
-var initialize = function(){
-    
-   // $.username.value = Alloy.CFG.user;
-    //$.password.value = Alloy.CFG.pass;    
+var initialize = function() {
+
+	// $.username.value = Alloy.CFG.user;
+	//$.password.value = Alloy.CFG.pass;
 };
 
 var init = function() {
-   initialize();
+	initialize();
 
 };
+
+$.login.addEventListener('android:back', function() {
+
+	var activity = Titanium.Android.currentActivity;
+	activity.finish();
+});
+
 init();
 navGroupModule.open();
 //$.login.open();
